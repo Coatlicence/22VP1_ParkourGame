0a0d8847a78afd3aff77f83022b027b79a765554
Coatlicence <nojdaknchick@gmail.com>
2023-11-26 18:07:42 +0300

    Добавлен игрок и прототипная логика

---
+++ Game/Assets/Scripts/_PlayerController.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class _PlayerController : MonoBehaviour
+{
+    [Header("References")]
+    [SerializeField] ISee see;
+
+    [SerializeField] IWalker walker;
+
+    [SerializeField] IJumper jumper;
+
+    void Start()
+    {
+        see = GetComponent<ISee>();
+        walker = GetComponent<IWalker>();
+        jumper = GetComponent<IJumper>();
+    }
+
+    protected void FixedUpdate()
+    {
+        if (walker) Move();
+    }
+
+    protected void Update()
+    {
+        if (jumper && Input.GetKeyDown(KeyCode.Space)) ; //jumper.Jump();
+    }
+
+    void Move()
+    {
+        float x = Input.GetAxisRaw("Horizontal");
+        float z = Input.GetAxisRaw("Vertical");
+
+        //walker.Move(x, z);
+    }
+}
