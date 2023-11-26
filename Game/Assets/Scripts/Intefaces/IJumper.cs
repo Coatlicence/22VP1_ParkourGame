c256ade86775e04dfaa7703391088ed3a0904121
xnuke2 <xmnikit@gmail.com>
2023-11-26 21:04:14 +0300

    Добавлен прыжок(нерабочий)

--- Game/Assets/Scripts/Intefaces/IJumper.cs
+++ Game/Assets/Scripts/Intefaces/IJumper.cs
@@ -1,18 +1,23 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+[RequireComponent(typeof(Rigidbody))]
 
 public class IJumper : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Rigidbody _rigidbody;
+    [SerializeField] private float _speed;
+    private void Start()
     {
-        
+        _rigidbody = GetComponent<Rigidbody>();
     }
-
-    // Update is called once per frame
     void Update()
     {
-        
+          if (Input.GetKeyDown(KeyCode.Space))
+        {
+            RigidbodyJamp();
+        }
+    }
+    private void RigidbodyJamp()
+    {
+        _rigidbody.AddForce(Vector3.up * _speed, ForceMode.VelocityChange);
     }
 }
