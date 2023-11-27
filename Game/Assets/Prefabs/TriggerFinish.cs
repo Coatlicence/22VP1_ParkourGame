820e744402b1b5576d34bd31ab33b0143492c6f9
Sagonash <ryazanskiy.maxim@yandex.ru>
2023-11-27 11:48:47 +0300

    Триггер?
    должен работать текст, но хз

--- Game/Assets/Prefabs/TriggerFinish.cs
+++ Game/Assets/Prefabs/TriggerFinish.cs
@@ -1,14 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using System;
+using UnityEngine.UI;
 
 public class TriggerFinish : MonoBehaviour
 {
+    public TMP_Text t;
+    private void Start()
+    {
+        t = GetComponent<TextMeshPro>();
+    }
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Player")
         {
-            //метод при достижении финиша
+            t.text = "Victory!";
         }
     }
 }
